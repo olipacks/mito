21c1ebfdcc3ac440ab92fc8d853befab187f33c2
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-03 19:01:16 -0600

    pass tests

--- tests/Feature/Http/Livewire/EditPostTest.php
+++ tests/Feature/Http/Livewire/EditPostTest.php
@@ -17,6 +17,15 @@ it('autosaves a draft when the markdown is updated.', function () {
     expect(Post::whereMarkdown('::markdown::')->exists())->toBeTrue();
 });
 
+it('disables autosave on published post', function () {
+    $this->draft->markAsPublished();
+
+    $this->livewire(EditPost::class, ['post' => $this->draft])
+        ->set('post.markdown', '::markdown::');
+
+    expect($this->draft->markdown)->not->toBe('::markdown::');
+});
+
 it('can create draft', function () {
     $this->livewire(EditPost::class, ['post' => $this->draft])
         ->call('createDraft');
@@ -78,4 +87,38 @@ it('can dispatch notify event on image upload', function () {
     $this->livewire(EditPost::class, ['post' => $this->draft])
         ->set('image', $image)
         ->assertDispatchedBrowserEvent('notify');
+});
+
+it('can save a draft', function () {
+    $this->livewire(EditPost::class, ['post' => $this->draft])
+        ->set('post.markdown', '::markdown::')
+        ->call('save');
+
+    expect($this->draft->fresh()->markdown)->toBe('::markdown::');
+});
+
+it('can save a published post', function () {
+    $publishedPost = tap($this->draft)->markAsPublished();
+
+    $this->livewire(EditPost::class, ['post' => $publishedPost])
+        ->set('post.markdown', '::markdown::')
+        ->call('save');
+
+    expect($publishedPost->fresh()->markdown)->toBe('::markdown::');
+});
+
+it('can publish a draft', function () {
+    $this->livewire(EditPost::class, ['post' => $this->draft])
+        ->call('publish');
+
+    expect($this->draft->fresh()->isPublished())->toBeTrue();
+});
+
+it('can unpublish a published post', function () {
+    $publishedPost = tap($this->draft)->markAsPublished();
+
+    $this->livewire(EditPost::class, ['post' => $publishedPost])
+        ->call('unpublish');
+
+    expect($publishedPost->fresh()->isDraft())->toBeTrue();
 });
