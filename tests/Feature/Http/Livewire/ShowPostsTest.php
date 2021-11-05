db2f152803962761f1579d4b56998ace5180e6c8
oliverds <oliverds@users.noreply.github.com>
2021-11-05 20:47:23 +0000

    Fix styling

--- tests/Feature/Http/Livewire/ShowPostsTest.php
+++ tests/Feature/Http/Livewire/ShowPostsTest.php
@@ -1,20 +1,14 @@
 <?php
 
-use Mito\Http\Livewire\ShowPosts;
-use Mito\Models\Post;
 
 it('it can show all drafts', function () {
-
 });
 
 it('it can filter by published posts', function () {
-
 });
 
 it('it can filter by drafts', function () {
-
 });
 
 it('it can create a new draft', function () {
-
 });
