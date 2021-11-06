895514882427b91370de36f2f14930659b6c7976
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-06 13:27:07 -0600

    Update post-list.blade.php

--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -8,32 +8,33 @@
     ],
 ])
 
-@if ($posts->count() > 0)
-    <div class="flex-shrink-0">
-        <div class="h-14 bg-white px-3 flex flex-col justify-center">
-            <div class="flex space-x-4">
-                @foreach ($links as list($title, $link, $active))
-                    <a
-                        href="{{ $link }}"
-                        class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-100 hover:text-gray-900' }} px-3.5 py-1.5 rounded-md text-sm font-medium"
-                    >
-                        {{ $title }}
-                    </a>
-                @endforeach
-            </div>
-        </div>
-
-        <div class="bg-white p-3">
-            <form wire:submit.prevent="createDraft">
-                <div class="flex flex-col">
-                    <x-mito::button color="white">
-                        <x-mito::icon.plus class="-ml-1 mr-2 h-4 w-4" />
-                        New Draft
-                    </x-mito::button>
-                </div>
-            </form>
+<div class="flex-shrink-0">
+    <div class="h-14 bg-white px-3 flex flex-col justify-center">
+        <div class="flex space-x-4">
+            @foreach ($links as list($title, $link, $active))
+                <a
+                    href="{{ $link }}"
+                    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-100 hover:text-gray-900' }} px-3.5 py-1.5 rounded-md text-sm font-medium"
+                >
+                    {{ $title }}
+                </a>
+            @endforeach
         </div>
     </div>
+
+    <div class="bg-white p-3">
+        <form wire:submit.prevent="createDraft">
+            <div class="flex flex-col">
+                <x-mito::button color="white">
+                    <x-mito::icon.plus class="-ml-1 mr-2 h-4 w-4" />
+                    New Draft
+                </x-mito::button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@if ($posts->count() > 0)
     <nav aria-label="Post list" class="min-h-0 flex-1 overflow-y-auto p-3">
         <ul class="space-y-1">
             @foreach($posts as $post)
@@ -49,7 +50,11 @@
                                 @endif
                             </a>
                         </div>
-                        <time datetime="2021-01-27T16:35" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ $post->created_at->diffForHumans(null, null, true) }}</time>
+                        @if ($post->isPublished())
+                            <time datetime="{{ $post->published_at->format('Y-m-d') }}" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ $post->published_at->diffForHumans(null, null, true) }}</time>
+                        @else
+                            <time datetime="{{ $post->created_at->format('Y-m-d') }}" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ $post->created_at->diffForHumans(null, null, true) }}</time>
+                        @endif
                     </div>
                     @unless ($post->isEmpty())
                         <div class="mt-1">
