f28b3c8f0348e45114b6c8d13d9f4e6062a724d8
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-06 16:26:35 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Mito\Http\Livewire\EditPost;
 use Mito\Http\Livewire\ShowPosts;
+use Mito\Http\Middleware\EnsureUserIsAuthorized;
 
 Route::prefix('mito')
-    ->middleware('web')
+    ->middleware(['web', EnsureUserIsAuthorized::class])
     ->group(function () {
         Route::redirect('/', '/mito/posts');
         Route::get('/posts', ShowPosts::class)->name('mito.posts.index');
