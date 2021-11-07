c07b6c1890b8c18fceedf4c812d289493f0bef33
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-06 19:10:03 -0600

    describe tests

---
+++ tests/Feature/Http/Livewire/DeletePostTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Mito\Http\Livewire\ManagePostSettings;
+use Mito\Models\Post;
+
+it('can delete a post', function () {
+
+});
+
+it('redirects to show posts after deletion', function () {
+
+});
