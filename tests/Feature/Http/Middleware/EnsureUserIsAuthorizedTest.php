e398297184bd15a0afa86d43b596b0724f7886d7
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-06 19:07:36 -0600

    describe tests

---
+++ tests/Feature/Http/Middleware/EnsureUserIsAuthorizedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+test('authorized users can access to mito', function () {
+
+});
+
+test('unauthorized users can not access to mito', function () {
+
+});
+
+test('anyone can access to mito on local enviroments', function () {
+
+});
