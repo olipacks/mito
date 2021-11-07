713894b44331ebfe1ef5f6fbbd9887b46c94eb37
oliverds <oliverds@users.noreply.github.com>
2021-11-07 01:08:25 +0000

    Fix styling

--- tests/Feature/Http/Middleware/EnsureUserIsAuthorizedTest.php
+++ tests/Feature/Http/Middleware/EnsureUserIsAuthorizedTest.php
@@ -1,13 +1,10 @@
 <?php
 
 test('authorized users can access to mito', function () {
-
 });
 
 test('unauthorized users can not access to mito', function () {
-
 });
 
 test('anyone can access to mito on local enviroments', function () {
-
 });
