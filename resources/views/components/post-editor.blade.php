7b589a2755347d000c33216bd8dca93c529577da
Oliver Servín <oliverds@users.noreply.github.com>
2021-11-22 20:16:21 -0600

    drag and drop images into editor

--- resources/views/components/post-editor.blade.php
+++ resources/views/components/post-editor.blade.php
@@ -6,7 +6,7 @@ $classes = 'text-sm w-full min-h-full overflow-hidden';
     <div class="bg-white pt-5 pb-20 shadow min-h-full flex">
         <div class="flex-1 min-h-full px-3 sm:flex sm:justify-center sm:items-baseline sm:px-6 lg:px-8 overflow-hidden">
             <div class="min-h-full sm:w-0 sm:flex-1 relative" style="max-width: 65ch;">
-                <div>
+                <file-attachment class="block">
                     <div class="{{ $classes }} whitespace-pre-wrap px-3 py-2 text-red-500 break-words invisible" style="font-family: 'Writer'">{{ $post->markdown }}</div>
                     <textarea
                         {{ $attributes->wire('model') }}
@@ -14,13 +14,20 @@ $classes = 'text-sm w-full min-h-full overflow-hidden';
                         x-on:keydown.meta.i.prevent="$refs.italic.click()"
                         x-on:keydown.meta.shift.period.prevent="$refs.quote.click()"
                         x-on:keydown.meta.k.prevent="$refs.link.click()"
+                        x-on:file-attachment-accepted.window="
+                            attachment = $event.detail?.attachments?.[0];
+
+                            if (! attachment || ! attachment.file) return;
+
+                            @this.upload('image', attachment.file);
+                        "
                         class="{{ $classes }} absolute inset-0 bg-transparent border-0 resize-none focus:ring-0"
                         style="font-family: 'Writer'"
                         placeholder="Write here."
                         id="markdown"
                         autofocus
                     ></textarea>
-                </div>
+                </file-attachment>
             </div>
         </div>
     </div>
